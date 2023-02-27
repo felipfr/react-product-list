6eb4ff71856ae5f7b9f96c833aad1445d261f028
Felipe Rodrigues <99354371+felipfr@users.noreply.github.com>
2023-02-27 16:27:31 -0300

    Updating Routes
     To use HashRoutes rather than BrowserRoutes, because GitHub Pages doesn’t support routers that use the HTML5 pushState history API under the hood (see https://create-react-app.dev/docs/deployment/#notes-on-client-side-routing).

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,18 +3,18 @@ import HomeScreen from "./Routes/HomeScreen";
 import NotFound from "./Routes/NotFound";
 import ProductScreen from "./Routes/ProductScreen";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export const RoutesApp = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<HomeScreen />} />
         <Route path="/contato" element={<ContactScreen />} />
         <Route path="/produto/:id" element={<ProductScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
